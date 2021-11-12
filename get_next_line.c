7262eec05c638bab2abb2a0537d15402d71e5079
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2021-11-12 15:14:55 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -1,6 +1,6 @@
 #include "get_next_line.h"
 #include <unistd.h>
-
+/*
 char	*ft_get_new_holder(char *holder)
 {
 	//printf ("\n----- GET NEW HOLDER ----\n");
@@ -90,4 +90,73 @@ char	*get_next_line(int fd)
 	if (bytes == 0 || !new_line)
 		return (NULL);
 	return (new_line);
+}*/
+size_t	ft_get_char_len(const char *str, char c)
+{
+	size_t	i;
+	
+	i = 0;
+	if (!str)
+		return (NULL);
+	while (str[i] != c && str[i])
+		i++;
+		if (str[i] == c)
+		{
+			i++;
+			break;
+		}
+	return (i);
+}
+
+char	*ft_nosenombre(char *holder, size_t start, size_t finish)
+{
+	char	*dst;
+	
+	dst = (char *)malloc(sizeof(char) * (finish - start + 1));
+	if (!dst)
+		return (NULL);
+	while (start < finish)
+		*dst++ = holder[start++]
+	*dst = '\0';
+	return (dst);
+}
+
+size_t	ft_check_char(const char *str, char c)
+{
+	if (!str)
+		return (0);
+	while (*str != c && *str)
+		str++;
+	return (*str == c);
+}
+
+char	*noseelnombre(char *str, size_t start, char c)
+{
+	char	*dst;
+	size_t	i;
+	
+	i = ft_get_char_len(holder, c);
+	dst = (char *)malloc(sizeof(char) * (i + 1));
+	if (!dst)
+		return (NULL);
+	while (i--)
+		*dst++ = *str++;
+	*dst = '\0';
+	return (dst);
+}
+
+char	*get_next_line(int fd)
+{
+	static char *holder;
+	
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (!holder)
+		holder = noseelnombre(holder, '\0');
+	while (!ft_check_char(holder, '\n'));
+	{
+	}
+	next_line = noseelnombre(holder, 0, '\n');
+	holder = ft_noseelnombre(holder, ft_get_char_len(holder, '\n') - 1, '\0');
+	return (next_line);
 }
