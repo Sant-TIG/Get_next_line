619a9de5c9ff5543949e1152023d436223b93d86
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2022-01-18 13:34:41 +0100

    Update get_next_line_bonus.h

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,1 +1,12 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
+# include <stdlib.h>
+
+size_t	ft_check_char(const char *str, int c);
+size_t	ft_get_char_len(const char *str, int c);
+char	*ft_strcpy(char *dst, const char *src, size_t dstsize);
+char	*ft_strdup(const char *str, size_t len);
+char	*get_next_line(int fd);
+
+#endif
