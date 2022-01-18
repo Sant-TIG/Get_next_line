dce556ce93a975dbf821d0ae12b321a5fe09e8a2
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2022-01-18 13:33:55 +0100

    Update get_next_line_utils_bonus.c

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -1,1 +1,45 @@
+#include "get_next_line.h"
 
+char	*ft_strdup(const char *str, size_t len)
+{
+	char	*dst;
+
+	dst = (char *)malloc(sizeof(char) * (len + 1));
+	if (!dst)
+		return (NULL);
+	ft_strcpy(dst, str, len + 1);
+	return (dst);
+}
+
+size_t	ft_get_char_len(const char *str, int c)
+{
+	size_t	i;
+
+	i = 0;
+	if (c == '\0')
+	{
+		while (str[i])
+			i++;
+		return (i);
+	}
+	while (str[i] != c)
+		i++;
+	return (i + 1);
+}
+
+size_t	ft_check_char(const char *str, int c)
+{
+	if (!str)
+		return (0);
+	while (*str && *str != c)
+		str++;
+	return (*str == c);
+}
+
+char	*ft_strcpy(char *dst, const char *src, size_t dstsize)
+{
+	while (*src && --dstsize)
+		*dst++ = *src++;
+	*dst = '\0';
+	return (dst);
+}
